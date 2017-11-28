1f30363f8ad60fb335949ee34a147e6277c114a3
retarf <retarf@draco.localdomain>
2017-11-28 12:10:46 +0100

    authentication part II

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
-
 from django import forms
+
+from .models import Profile
 
 
 class LoginForm(forms.Form):
@@ -22,3 +23,17 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise form.ValidationError('Hasła nie są identyczne.')
             return cd['password2']
+
+
+class UserEditForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+
+class ProfileEditForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
--- account/urls.py
+++ account/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     url(r'^password-reset/complete/$', auth_views.password_reset_complete,
         name='password_reset_complete'),
     url(r'^register/$', views.register, name='register'),
+    url(r'^edit/$', views.edit, name='edit'),
 ]
